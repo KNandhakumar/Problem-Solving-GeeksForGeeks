0a4d2cee4e9a2a10c715dfae2f2cc5ab3c5cea7d
Nandhakumar <nknandhakumar64@gmail.com>
2024-09-15 21:40:07 +0530

    Patterns : Pattern C

---
+++ GeeksForGeeks/Patterns/patternC.java
@@ -0,0 +1,22 @@
+package GeeksForGeeks.Patterns;
+
+public class patternC {
+    public static void main(String[] args) {
+        //  *
+        //  * *
+        //  * * *
+        //  * * * *
+        //  * * * * *
+        int n = 5;
+        patternC(n);
+    }
+
+    static void patternC(int n){
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
